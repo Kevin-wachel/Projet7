dac9c86fd445e7d9540e1e29da3cfd28cc5da80d
Kevin-wachel <kevwachel@gmail.com>
2020-11-25 14:20:25 +0100

    Création des routes ne fonctionne pas encore.

---
+++ backend/controllers/message.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+
+
+// Création d'un message
+
+
+// Voir tout les messages
+
+
+// Voir les réponses
+
+
+// Modifier les messages
+
+
+// Supprimer les messages
+
+
+// Like
+
+
+// Dislike 
---
+++ backend/controllers/user.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../models/index');
+
+const User = require('../models/user');
+
+// Inscription
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({ 
+        username: req.body.username,
+        email: req.body.email,
+        password: hash
+      });
+      db.sequelize.query(`INSERT INTO users(username, email, password) VALUES('${user.username}','${user.email}','${user.password}')`)
+        .then(() => res.status(201).json({ message: 'Votre compte à bien été créer !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+  .catch(error => res.status(500).json({ message : "Il y a un problème" }));
+};
+
+// Connexion
+exports.login = (req, res, next) => {
+  db.User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).send({ error });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+// Tout les utilisateurs
+exports.getAllUsers = (req, res, next) => {
+  db.sequelize.query(`SELECT id, username, isAdmin, bio, email FROM users`)
+  .then(() => res.status(200).json({ results }))
+  .catch(error => res.status(400).json({ error }));
+};
+
+// Supprimer un utilisateur
+exports.deleteUser = (req, res, next) => {
+  db.sequelize.query(`DELETE FROM users WHERE id ='${req.params.id}' `)
+  .then(() => res.status(200).json({ message: 'Compte supprimé !'}))
+  .catch(error => res.status(400).json({ error }));
+};
