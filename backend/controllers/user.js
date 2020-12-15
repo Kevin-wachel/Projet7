aaaacf9ebf2d6d3571ea24b1022d87be26ca7d6d
Kevin-wachel <kevwachel@gmail.com>
2020-12-15 13:29:46 +0100

    Route user ok sauf delete.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,61 +1,70 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const sequelize = require('../config/connexiondb');
-const { Sequelize, DataTypes } = require('sequelize');
-const User = require('../models/user') (sequelize, DataTypes);
+const { User } = require('../models');
 
 // Inscription
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({ 
-        username: req.body.username,
-        email: req.body.email,
-        password: hash
+  .then(hash => {
+    const user = {
+      username: req.body.username,
+      email: req.body.email,
+      password: hash
+      };
+      User.create(user)
+      .then(user => {
+        res.send({ user, message : "Utilisateur bien enregistré" });       
+      })
+      .catch(err => {
+        res.status(500).send({ message: err.message || "Une erreur s'est produite lors de la création de l'utilisateur." });
       });
-      sequelize.query(`INSERT INTO users(username, email, password) VALUES('${user.username}','${user.email}','${user.password}')`)
-        .then(() => res.status(201).json({ message: 'Votre compte à bien été créer !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-  .catch(error => res.status(500).json({ message : "Il y a un problème" }));
-};
-
-// Connexion
-exports.login = (req, res, next) => {
-  db.User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).send({ error });
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-              { userId: user._id },
-              'RANDOM_TOKEN_SECRET',
-              { expiresIn: '24h' }
-            )
-          });
-        })
-        .catch(error => res.status(500).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
 
+// Connexion
+exports.login = (req, res, next) => {
+  User.findOne({
+    where: { email: req.body.email }
+  })
+  .then(user => {
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+    bcrypt.compare(req.body.password, user.password)
+    .then(valid => {
+      if (!valid) {
+        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+      }
+      res.status(200).json({
+        userId: user.id,
+        token: jwt.sign(
+          { userId: user.id },
+          'RANDOM_TOKEN_SECRET',
+          { expiresIn: '24h' }
+        )
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
+};
+
 // Tout les utilisateurs
 exports.getAllUsers = (req, res, next) => {
-  sequelize.query(`SELECT id, username, isAdmin, bio, email FROM users`)
-  .then(() => res.status(200).json({ results }))
-  .catch(error => res.status(400).json({ error }));
+  User.findAll()
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Il y a un problème" });
+    });
 };
 
 // Supprimer un utilisateur
 exports.deleteUser = (req, res, next) => {
-  sequelize.query(`DELETE FROM users WHERE id ='${req.params.id}' `)
-  .then(() => res.status(200).json({ message: 'Compte supprimé !'}))
+  User.deleteOne({ id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Utilisateur supprimé !'}))
   .catch(error => res.status(400).json({ error }));
 };
