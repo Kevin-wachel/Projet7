4b9f4b0d0c41fc86ba902486756c17346f73537d
Kevin-wachel <kevwachel@gmail.com>
2021-05-30 18:48:22 +0200

    Routes allmessageetcommentaire supprimer.

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -24,17 +24,6 @@ exports.getAllMessages = (req, res, next) => {
   });
 };
 
-// Voir tout les messages et les commentaires
-exports.getAllMessagesCommentaires = (req, res, next) => {
-  sql.query("SELECT messages.id, messages.contentMessage, messages.attachment, messages.likes, users.username, commentaires.contentCommentaire, commentaires.messageId FROM messages INNER JOIN users ON messages.userId = users.id CROSS JOIN commentaires ON messages.id = commentaires.messageId ORDER BY messages.id DESC", 
-  function (error, results, fields) {
-    if (error) {
-      return res.status(400).json(error);
-    }
-    return res.status(200).json({ results });
-  });
-};
-
 // Supprimer les messages
 exports.deleteMessage = (req, res, next) => {
   sql.query(`DELETE FROM messages WHERE id=${req.params.id}`, req.params.id,
